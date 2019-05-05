96ab60ad87b54fb240825fcf94d3d2fd9d9e2fe7
evanfeng <evanfeng@pku.edu.cn>
2019-05-05 18:09:43 +0800

    fix trigger1

--- trigger1.sql
+++ trigger1.sql
@@ -33,9 +33,6 @@ create table stk.trans
 )
 go
 
-drop trigger stk.TRIG_trans_update
-go
-
 -- Create trigger: update my_stock after records insterted into trans
 create trigger TRIG_trans_update
 	on stk.trans
@@ -86,7 +83,7 @@ begin
 	)
 
 	insert into @tmp
-	select t1.stock_id, t1.trans_id, t1.amount, t1.price, sum(t2.amount)
+	select t1.stock_id, t1.trans_id, t1.amount, t1.price, isnull(sum(t2.amount), 0)
 	from stk.trans t1, stk.trans t2
 	where t2.date >= t1.date
 	and t1.stock_id = t2.stock_id
@@ -96,14 +93,28 @@ begin
 
 	update stk.my_stock
 	set profit =
-	( select isnull(sum(amount * price), 0) from stk.trans
-	  where sell_or_buy = 'sell' and stk.trans.stock_id = stk.my_stock.stock_id )
-	- ( select isnull(sum(amount * price), 0) from stk.trans
-	    where sell_or_buy = 'buy' and stk.trans.stock_id = stk.my_stock.stock_id )
-	+ ( select isnull(sum(amount * price), 0) from @tmp where stk.my_stock.stock_id = stock_id and acc_amount <= volume)
-	+ ( select sum((tmp.amount - tmp.acc_amount + stk.my_stock.volume) * tmp.price)
+	( select isnull(sum(amount * price), 0)
+	  from stk.trans
+	  where sell_or_buy = 'sell'
+	  and stk.trans.stock_id = stk.my_stock.stock_id )
+	- ( select isnull(sum(amount * price), 0)
+	    from stk.trans
+	    where sell_or_buy = 'buy'
+		and stk.trans.stock_id = stk.my_stock.stock_id )
+	+ ( select isnull(sum(amount * price), 0)
+	    from @tmp
+		where stk.my_stock.stock_id = stock_id
+		and acc_amount <= stk.my_stock.volume)
+	+ ( select isnull(sum((tmp.amount - tmp.acc_amount) * tmp.price), 0)
 		from @tmp tmp
-		where stk.my_stock.stock_id = tmp.stock_id and tmp.acc_amount > stk.my_stock.volume and tmp.acc_amount - tmp.amount <= stk.my_stock.volume )
+		where stk.my_stock.stock_id = tmp.stock_id
+		and tmp.acc_amount > stk.my_stock.volume
+		and tmp.acc_amount - tmp.amount <= stk.my_stock.volume )
+	+ volume * ( select isnull(sum(tmp.price), 0)
+		from @tmp tmp
+		where stk.my_stock.stock_id = tmp.stock_id
+		and tmp.acc_amount > stk.my_stock.volume
+		and tmp.acc_amount - tmp.amount <= stk.my_stock.volume )
 
 end
 go
@@ -113,13 +124,13 @@ delete from stk.my_stock
 delete from stk.trans
 insert into stk.my_stock values ('001', 0, 0, 0);
 insert into stk.my_stock values ('002', 0, 0, 0);
-insert into stk.my_stock values ('003', 0, 0, 0);
-insert into stk.my_stock values ('004', 0, 0, 0);
-insert into stk.trans values ('T1', '001', '5-1-2019', 2.3, 10, 'buy');
-insert into stk.trans values ('T2', '001', '5-2-2019', 2.5, 10, 'buy');
-insert into stk.trans values ('T3', '002', '5-3-2019', 2.0, 5, 'buy');
-insert into stk.trans values ('T4', '002', '5-4-2019', 3.0, 10, 'buy');
-insert into stk.trans values ('T5', '002', '5-5-2019', 4.0, 5, 'sell');
-
+insert into stk.trans values ('1', '001', '5-1-2019', 10, 1000, 'buy');
+insert into stk.trans values ('2', '001', '5-2-2019', 11, 500, 'buy');
+insert into stk.trans values ('3', '001', '5-3-2019', 12, 800, 'sell');
+--insert into stk.trans values ('4', '001', '5-4-2019', 12, 1000, 'sell');
+insert into stk.trans values ('5', '001', '5-5-2019', 9, 1000, 'buy');
+insert into stk.trans values ('6', '001', '5-6-2019', 12, 800, 'sell');
+insert into stk.trans values ('7', '001', '5-7-2019', 7, 800, 'sell');
 select * from stk.my_stock;
 select * from stk.trans;
+go
